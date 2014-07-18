dfd85c8a56e90c537a04e170d6fdc9f6ed88946c
tsukuyomip <ab0312uo@gmail.com>
2014-07-18 16:07:05 +0900

    fizzbuzz.py を修正
    エラーを修正．if -> elif

--- sandbox/fizzbuzz.py
+++ sandbox/fizzbuzz.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     for i in range(1, 20):
         if i%3 == 0 and i%5 == 0:
             print "fizzbuss"
-        if i%3 == 0:
+        elif i%3 == 0:
             print "fizz"
         elif i%5 == 0:
             print "buzz"
